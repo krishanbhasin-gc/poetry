ef245a22290d0605d0c9bd56e4b43c6adf9d480a
Krishan Bhasin <krishanbhasin@gocardless.com>
2023-10-23 17:44:12 +0100

    Update exception message to be clear env use is a poetry command

--- src/poetry/utils/env/exceptions.py
+++ src/poetry/utils/env/exceptions.py
@@ -54,7 +54,7 @@ class NoCompatiblePythonVersionFound(EnvError):
             message = (
                 "Poetry was unable to find a compatible version. "
                 "If you have one, you can explicitly use it "
-                'via the "env use" command.'
+                'via the "poetry env use <version>" command.'
             )
 
         super().__init__(message)
@@ -65,7 +65,7 @@ class InvalidCurrentPythonVersionError(EnvError):
         message = (
             f"Current Python version ({given}) "
             f"is not allowed by the project ({expected}).\n"
-            'Please change python executable via the "poetry env use" command.'
+            'Please change python executable via the "poetry env use <version>" command.'
         )
 
         super().__init__(message)
